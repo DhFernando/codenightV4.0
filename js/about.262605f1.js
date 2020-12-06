8acbd0a3f6b3991182cae124d4eeec1dfdf09411
dhfernando <hdilshan.fernando11@gmail.com>
2020-12-07 00:10:27 +0530

    deploy

---
+++ js/about.262605f1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],c={name:"About",data:function(){return{}},methods:{},computed:{},created:function(){}},o=c,s=n("2877"),r=Object(s["a"])(o,a,u,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.262605f1.js.map
